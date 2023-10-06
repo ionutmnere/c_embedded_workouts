a4c451046da8ccb2aaf8418b931cea8cccbdeb29
Ionut Mnere <ionut.mnere@gmail.com>
2023-10-06 15:42:42 +0300

    Variable_Datatypes: added datatypes example

---
+++ Ionut_Mnere/Getting_familiar_with_C/Variable_Datatypes/datatypes.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int int_variable_1 = 10;
+	int int_variable_2 = 20;
+	float float_variable_1 = 10.11;
+	float float_variable_2 = 20.22;
+    char char_variable_1 = 'C';
+	char char_variable_2 = 'd';
+	printf("Printing the value of the first integer variable: %d \n", int_variable_1);
+	printf("Printing the value of the second integer variable: %d \n", int_variable_2);
+    printf("Printing the value of the first float variable: %f \n", float_variable_1);
+	printf("Printing the value of the second float variable: %f \n", float_variable_2);
+	printf("Printing the value of the first char variable: %c \n", char_variable_1);
+	printf("Printing the value of the second char variable: %c \n", char_variable_2);
+	printf("Printing the value of the first char variable: %d \n", char_variable_1);
+	printf("Printing the value of the second char variable: %d \n", char_variable_2);
+
+	// TODO
+
+	return EXIT_SUCCESS;
+}
