5347225cdf634f146d85d113f2e7fc95f1d4e832
Ionut Mnere <ionut.mnere@gmail.com>
2023-10-09 14:52:33 +0300

    Control Structures: if conditions example

---
+++ Ionut_Mnere/Logical_Programming_in_C/Getting_Started_with_Control_Structures/if_consitions.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	if(0) {
+		printf("The condition if (0) evaluates to true \n");
+	}
+	else {
+		printf("The condition if (0) evaluates to false \n");	
+	}
+
+	if(1) {
+	printf("The condition if (1) evaluates to true \n");
+	}
+	else {
+		printf("The condition if (1) evaluates to false \n");	
+	}
+	return EXIT_SUCCESS;
+}
